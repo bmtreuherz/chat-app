e2ab00cdfcf1a5f136fa2658548033c00aa577a6
bmtreuherz <bradlemtreuherz@gmail.com>
2015-06-23 21:59:58 -0400

    Finished scaffolding and added bootswatch:paper

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by bradley on 6/23/15.
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/home/home.js
@@ -0,0 +1,17 @@
+/**
+ * Created by bradley on 6/23/15.
+ */
+Session.setDefault('counter', 0);
+
+Template.home.helpers({
+    counter: function () {
+        return Session.get('counter');
+    }
+});
+
+Template.home.events({
+    'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+    }
+});
